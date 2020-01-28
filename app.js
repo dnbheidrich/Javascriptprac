2a176fc0a845d5fc9e775388edbb1f325dadb63b
Dieter Heidrich <dnbheidrich@gmail.com>
2020-01-28 11:41:37 -0700

    changes

--- app.js
+++ app.js
@@ -32,3 +32,36 @@ let family ={
 },
 }
 console.log(family.brother.age)
+
+
+
+let penn = {
+ tip:{
+   color: "silver and green",
+   material:"tin and rubber",
+   use:"holds the tip of the ink cartridge in position when the cartridge is extended for writing.",
+   parts: 2,
+
+ },
+ barrel: {
+  color:"silver and green",
+  material:"tin",
+  use:"The pen barrel houses the inner parts of the pen.",
+  parts: 1,
+
+ },
+ spring: {
+  color:"silver",
+  material:"steel",
+  use:"Keeps the cartridge retracted until ready for use.",
+  parts: 1
+ },
+
+ inkChamber: {
+  color:"black",
+  material:"plastic and ink",
+  use:" Whether it’s ballpoint, gel, rollerball, or hybrid, refillable or single use, ink’s stored in the cartridge. When the cartridge is extended, the pen’s ready to write.",
+ },
+ 
+}
+console.log(penn.barrel.color)
