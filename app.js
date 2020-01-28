1a44ec66cb4e2330abd8b288c5cf7748de9a2628
Dieter Heidrich <dnbheidrich@gmail.com>
2020-01-28 09:12:02 -0700

    changes

---
+++ app.js
@@ -0,0 +1,34 @@
+
+let family ={
+
+
+ brother: { 
+  name:"Dieter",
+  age: 23,
+  eyeColor: "Brown",
+  hairColor: "Brown"
+},
+
+
+ sister: {
+  name: "Lily",
+  age: 19,
+  eyeColor: "Blue",
+  hairColor: "Brown"
+},
+
+
+ mom: {
+  name: "Wendy",
+  age: 56,
+  eyeColor: "Blue",
+  hairColor: "Brown"
+},
+ dad: {
+  name: "Mark",
+  age: 55,
+  eyeColor: "Brown",
+  hairColor: "Brown"
+},
+}
+console.log(family.brother.age)
